c953ab5eaf625d38696373af2c6dcb208c1ed787
elias <eliastello.dev@gmail.com>
2024-07-30 12:56:40 -0300

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+// const Gpio = require("pigpio-mock").Gpio;
+
+// const led = new Gpio(13, { mode: Gpio.OUTPUT });
+// let dutyCycle = 0;
+
+// setInterval(function () {
+//   led.pwmWrite(dutyCycle);
+
+//   dutyCycle += 50;
+//   if (dutyCycle > 255) {
+//     dutyCycle = 0;
+//   }
+// }, 500);
+
 function App() {
   const [encendido, setEncendido] = useState(false);
 
